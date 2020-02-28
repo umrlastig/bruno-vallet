b1fdcd8622b3a6684b36cd30a03dd9a0c361178b
brunovallet <bruno.vallet@ign.fr>
2020-02-28 19:02:00 +0100

    Forgotten files

---
+++ cours/PPMD/hidden/contours.py
@@ -0,0 +1,114 @@
+﻿# input/output
+img_filename = "batG.png"
+sigma = 1.
+seuil_bas = 0.02
+seuil_haut = 0.1
+canny_filename = 'batG.cannyH.png'
+lapl_filename = 'batG.laplH.png'
+
+print('Initialisation')
+import numpy as np
+import scipy as sp
+import scipy.misc as sm
+import skimage as sf
+
+def snap(A):
+    return (A>1).astype(int) - (A<-1).astype(int)
+
+def Cluster(mask):
+    processed = np.zeros(image.shape, dtype=np.bool)
+    ret = []
+    for l in range(1,image.shape[0]-1):
+        for c in range(1,image.shape[1]-1):
+            seed = (l,c)
+            if not processed[seed] and mask[seed]:
+                cluster = []
+                pile = [seed]
+                processed[seed] = 1
+                while len(pile) > 0:
+                    pix = pile[-1]
+                    pile.pop()
+                    cluster.append(pix)
+                    nn = 0
+                    for dl in range(-1,2):
+                        for dc in range(-1,2):
+                            new_pix = (pix[0]+dl,pix[1]+dc)
+                            if not processed[new_pix] and mask[new_pix]:
+                                pile.append(new_pix)
+                                processed[new_pix] = 1
+                                nn += 1
+                ret.append(cluster)
+    return ret
+
+def Hyst(mask, grad_norm, seuil_haut):
+    clusters = Cluster(mask)
+    ret = np.zeros(image.shape, dtype=np.bool)
+    for cluster in clusters:
+        ok = 0
+        for pix in cluster:
+            if grad_norm[pix]>seuil_haut:
+                ok = 1
+        if ok:
+            for pix in cluster:
+                ret[pix]=1
+    return ret
+                
+def ContoursCanny(image, sigma, seuil_bas, seuil_haut):
+    gauss_img = sf.filter.gaussian_filter(image, sigma=sigma, mode='nearest').astype(float)
+    grad_c = np.array([[-1, 1]])
+    grad_l = grad_c.transpose()
+    grad_c_img = sp.ndimage.convolve(gauss_img, grad_c)
+    sm.imsave(img_filename+'.gc.jpg', grad_c_img)
+    grad_l_img = sp.ndimage.convolve(gauss_img, grad_l)
+    sm.imsave(img_filename+'.gl.jpg', grad_l_img)
+    grad_norm = np.sqrt(
+       np.multiply(grad_c_img, grad_c_img)+
+       np.multiply(grad_l_img, grad_l_img))
+    sm.imsave(img_filename+'.gnorm.jpg', grad_norm)
+    grad_s = 0.382683432*grad_norm
+    gs_c = snap(grad_c_img / grad_s)
+    #sm.imsave(img_filename+'.gsc.jpg', gs_c)
+    gs_l = snap(grad_l_img / grad_s)
+    #sm.imsave(img_filename+'.gsl.jpg', gs_l)
+    contours = np.zeros(image.shape, dtype=np.bool)
+    for l in range(1,image.shape[0]-1):
+        for c in range(1,image.shape[1]-1):
+            contours[l,c] = (grad_norm[l,c] > seuil_bas
+            and grad_norm[l,c]>grad_norm[l+gs_l[l,c]][c+gs_c[l,c]]
+            and grad_norm[l,c]>grad_norm[l-gs_l[l,c]][c-gs_c[l,c]])
+    if seuil_haut > seuil_bas:
+        return Hyst(contours, grad_norm, seuil_haut)
+    return contours
+    
+def ContoursLaplace(image, sigma, seuil_bas, seuil_haut):
+    gauss_img = sf.filter.gaussian_filter(image, sigma=sigma, mode='nearest').astype(float)
+    L = np.array([[1., 2., 1.],[2., -12., 2.],[1., 2., 1.]])/3.
+    L_img = sp.ndimage.convolve(gauss_img, L)
+    sm.imsave(img_filename+'.L.jpg', L_img)
+    contours = np.zeros(image.shape, dtype=np.bool)
+    pseudo_grad = seuil_bas * np.ones(image.shape)
+    for l in range(1,image.shape[0]-1):
+        for c in range(1,image.shape[1]-1):
+            cur_L = L_img[l,c]
+            if cur_L>0:
+                for dl in range(-1,2):
+                    for dc in range(-1,2):
+                        neigh_L = L_img[l+dl,c+dc]
+                        pg = abs(gauss_img[l,c] - gauss_img[l+dl,c+dc])
+                        if neigh_L<0 and pg > pseudo_grad[l,c]:
+                            contours[l,c] = 1
+                            pseudo_grad[l,c] = pg
+    if seuil_haut > seuil_bas:
+        return Hyst(contours, grad_norm, seuil_haut)
+    return contours  
+    
+print('Reading %s' % img_filename)
+image = sm.imread(img_filename)
+print(image.shape)
+canny = ContoursCanny(image, sigma, seuil_bas, seuil_haut)
+print('Writing %s' % canny_filename)
+sm.imsave(canny_filename, canny)
+laplace = ContoursLaplace(image, sigma, seuil_bas, seuil_haut)
+print('Writing %s' % lapl_filename)
+sm.imsave(lapl_filename, laplace)
+
---
+++ cours/PPMD/hidden/opti.py
@@ -0,0 +1,103 @@
+﻿# input/output
+img_filename = "lena.jpg"
+filt_img_filename = "lena_filt.jpg"
+mask_img_filename = "lenaMask.png"
+l = 0.001
+smooth = 0
+
+print('Initialisation')
+import numpy as np
+import scipy as sp
+import scipy.misc as sm
+import scipy.sparse.linalg as ssl
+
+def Gc(nl, nc):
+    ret = np.zeros(((nc-1)*nl,nc*nl))
+    i_l=0
+    for l in range(nl):
+        for c in range(nc-1):
+            ret[i_l, c + 1 + nc * l] = 1
+            ret[i_l, c + nc * l] = -1
+            i_l+=1
+    return ret
+
+def Gl(nl, nc):
+    ret = np.zeros(((nl-1)*nc,nc*nl))
+    i_l=0
+    for l in range(nl-1):
+        for c in range(nc):
+            ret[i_l, c + nc * (l+1)] = 1
+            ret[i_l, c + nc * l] = -1
+            i_l+=1
+    return ret
+
+def Gcs(nl, nc):
+    data = []
+    row_ind = []
+    col_ind = []
+    i_l=0
+    for l in range(nl):
+        for c in range(nc-1):
+            data.append(-1)
+            row_ind.append(i_l)
+            col_ind.append(c + nc * l)
+            data.append(1)
+            row_ind.append(i_l)
+            col_ind.append(c + 1 + nc * l)
+            i_l+=1
+    return sp.sparse.csr_matrix((data, (row_ind, col_ind)))
+
+def Gls(nl, nc):
+    data = []
+    row_ind = []
+    col_ind = []
+    i_l=0
+    for l in range(nl-1):
+        for c in range(nc):
+            data.append(-1)
+            row_ind.append(i_l)
+            col_ind.append(c + nc * l)
+            data.append(1)
+            row_ind.append(i_l)
+            col_ind.append(c + nc * (l+1))
+            i_l+=1
+    return sp.sparse.csr_matrix((data, (row_ind, col_ind)))
+
+def sparse_eye(n):
+    return sp.sparse.csr_matrix((np.ones(n), (range(n), range(n))))
+
+def sparse_diag(v):
+    n=v.shape[0]
+    return sp.sparse.csr_matrix((v, (range(n), range(n))))
+
+print('Reading %s' % img_filename)
+image = sm.imread(img_filename).astype(float)
+print(image.shape)
+#image = image[:10,:10]
+
+grad_c = Gcs(image.shape[0],image.shape[1])
+grad_l = Gls(image.shape[0],image.shape[1])
+
+G = np.dot(grad_c.transpose(),grad_c)+np.dot(grad_l.transpose(),grad_l)
+f = np.reshape(image, image.shape[0]*image.shape[1])
+
+if smooth:
+    A = sparse_eye(G.shape[0])+l*G
+    u = ssl.spsolve(A, f)
+    img_filt = np.reshape(u, (image.shape[0],image.shape[1])).astype(uint8)
+
+else:
+    # mask
+    print('Reading %s' % mask_img_filename)
+    mask = sm.imread(mask_img_filename)
+    print(mask.shape)
+    mask = mask[:image.shape[0], :image.shape[1]].astype(float)/255
+    mask_v = np.reshape(mask, mask.shape[0] * mask.shape[1])
+    M = sparse_diag(mask_v)
+    A = M + l*G
+    b = M.dot(f)
+    u = ssl.spsolve(A, b)
+    img_filt = np.reshape(u, (image.shape[0],image.shape[1])).astype(np.uint8)
+    
+print('Saving %s' % filt_img_filename)
+sm.imsave(filt_img_filename, img_filt)
