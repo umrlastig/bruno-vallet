40d93779f731587debbce8eb6978fc6f1da916b2
Bruno <bruno.vallet@ign.fr>
2021-02-18 16:47:41 +0100

    Adapted contours.py to Python 3.8

--- cours/PPMD/hidden/contours.py
+++ cours/PPMD/hidden/contours.py
@@ -9,8 +9,13 @@ lapl_filename = 'batG.laplH.png'
 print('Initialisation')
 import numpy as np
 import scipy as sp
-import scipy.misc as sm
+#import scipy.misc as sm
 import skimage as sf
+import skimage.filters as filters
+import imageio
+from scipy import ndimage
+import matplotlib.pyplot as plt
+
 
 def snap(A):
     return (A>1).astype(int) - (A<-1).astype(int)
@@ -54,22 +59,23 @@ def Hyst(mask, grad_norm, seuil_haut):
     return ret
                 
 def ContoursCanny(image, sigma, seuil_bas, seuil_haut):
-    gauss_img = sf.filter.gaussian_filter(image, sigma=sigma, mode='nearest').astype(float)
+    #gauss_img = sf.filter.gaussian_filter(image, sigma=sigma, mode='nearest').astype(float)
+    gauss_img = filters.gaussian(image, sigma=sigma, mode='nearest', preserve_range=True)
     grad_c = np.array([[-1, 1]])
     grad_l = grad_c.transpose()
     grad_c_img = sp.ndimage.convolve(gauss_img, grad_c)
-    sm.imsave(img_filename+'.gc.jpg', grad_c_img)
+    imageio.imwrite(img_filename + '.gc.jpg', grad_c_img)
     grad_l_img = sp.ndimage.convolve(gauss_img, grad_l)
-    sm.imsave(img_filename+'.gl.jpg', grad_l_img)
+    imageio.imwrite(img_filename + '.gl.jpg', grad_l_img)
     grad_norm = np.sqrt(
        np.multiply(grad_c_img, grad_c_img)+
        np.multiply(grad_l_img, grad_l_img))
-    sm.imsave(img_filename+'.gnorm.jpg', grad_norm)
+    imageio.imwrite(img_filename+'.gnorm.jpg', grad_norm)
     grad_s = 0.382683432*grad_norm
-    gs_c = snap(grad_c_img / grad_s)
-    #sm.imsave(img_filename+'.gsc.jpg', gs_c)
-    gs_l = snap(grad_l_img / grad_s)
-    #sm.imsave(img_filename+'.gsl.jpg', gs_l)
+    gs_c = snap(grad_c_img / (grad_s+0.001))
+    imageio.imwrite(img_filename + '.gsc.jpg', gs_c)
+    gs_l = snap(grad_l_img / (grad_s+0.001))
+    imageio.imwrite(img_filename + '.gsl.jpg', gs_l)
     contours = np.zeros(image.shape, dtype=np.bool)
     for l in range(1,image.shape[0]-1):
         for c in range(1,image.shape[1]-1):
@@ -77,14 +83,14 @@ def ContoursCanny(image, sigma, seuil_bas, seuil_haut):
             and grad_norm[l,c]>grad_norm[l+gs_l[l,c]][c+gs_c[l,c]]
             and grad_norm[l,c]>grad_norm[l-gs_l[l,c]][c-gs_c[l,c]])
     if seuil_haut > seuil_bas:
-        return Hyst(contours, grad_norm, seuil_haut)
-    return contours
+        return grad_norm, Hyst(contours, grad_norm, seuil_haut)
+    return grad_norm, contours
     
 def ContoursLaplace(image, sigma, seuil_bas, seuil_haut):
-    gauss_img = sf.filter.gaussian_filter(image, sigma=sigma, mode='nearest').astype(float)
+    gauss_img = filters.gaussian(image, sigma=sigma, mode='nearest').astype(float)
     L = np.array([[1., 2., 1.],[2., -12., 2.],[1., 2., 1.]])/3.
     L_img = sp.ndimage.convolve(gauss_img, L)
-    sm.imsave(img_filename+'.L.jpg', L_img)
+    imageio.imwrite(img_filename + '.L.jpg', L_img)
     contours = np.zeros(image.shape, dtype=np.bool)
     pseudo_grad = seuil_bas * np.ones(image.shape)
     for l in range(1,image.shape[0]-1):
@@ -103,12 +109,28 @@ def ContoursLaplace(image, sigma, seuil_bas, seuil_haut):
     return contours  
     
 print('Reading %s' % img_filename)
-image = sm.imread(img_filename)
+image = imageio.imread(img_filename)
 print(image.shape)
-canny = ContoursCanny(image, sigma, seuil_bas, seuil_haut)
+grad_norm, canny = ContoursCanny(image, sigma, seuil_bas, seuil_haut)
 print('Writing %s' % canny_filename)
-sm.imsave(canny_filename, canny)
+imageio.imwrite(canny_filename, sf.img_as_uint(canny))
 laplace = ContoursLaplace(image, sigma, seuil_bas, seuil_haut)
 print('Writing %s' % lapl_filename)
-sm.imsave(lapl_filename, laplace)
+imageio.imwrite(lapl_filename, sf.img_as_uint(laplace))
 
+#imageio.imwrite('raster_GreyClosing.tif', close_img)  # enregistre les résultats dans un fichier
+
+plt.figure()
+plt.subplot(221)
+plt.imshow(image)
+plt.title("image")
+plt.subplot(222)
+plt.imshow(grad_norm)
+plt.title("grad_norm")
+plt.subplot(223)
+plt.imshow(canny)
+plt.title("canny")
+plt.subplot(224)
+plt.imshow(laplace)
+plt.title("laplace")
+plt.show()
